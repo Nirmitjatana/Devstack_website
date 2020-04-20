683a637c55d25b08e8753de07e21c88035c2fcf4
Nirmitjatana <Nirmitjatana@users.noreply.github.com>
2020-04-20 21:53:55 +0530

    major commit

--- JS/nav.js
+++ JS/nav.js
@@ -1,6 +1,9 @@
 const hamburger = document.querySelector(".hamburger");
 const navlinks = document.querySelector('.nav-links');
 const links = document.querySelector('.nav-links li');
+const tabs =document.querySelector('.nav-links li a');
+const nj = document.querySelector('.page-section');
+var tab;
 
 hamburger.addEventListener("click", ()=>{
     navlinks.classList.toggle("open");
@@ -13,3 +16,40 @@ $(document).ready(function(){
       $(this).toggleClass("is-active");
     });
   });
+
+nj.addEventListener("click", ()=>{
+  $("#hamburger-6").click()
+  console.log("nj")
+})
+
+
+$(document).ready(function() {
+  $('a[href*=#]').bind('click', function(e) {
+      e.preventDefault();
+
+      var target = $(this).attr("href");
+
+      
+      $('html, body').stop().animate({
+          scrollTop: $(target).offset().top
+      }, 300, function() {
+          location.hash = target; 
+      });
+      $("#hamburger-6").click()
+      return false;
+  });
+});
+
+$(window).scroll(function() {
+  var scrollDistance = $(window).scrollTop();
+  $('.page-section').each(function(i) {
+      if ($(this).position().top <= scrollDistance) {
+          $('.nav-links li a.active').removeClass('active');
+          $('.nav-links li a').eq(i).addClass('active');
+          tab=1;
+      } 
+
+  });
+}).scroll();
+
+
