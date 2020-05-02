b97790d497bd3d5b1e9f82f00c6d881ed13ae224
Nirmitjatana <Nirmitjatana@users.noreply.github.com>
2020-05-02 19:40:15 +0530

    sponsor

--- JS/nav.js
+++ JS/nav.js
@@ -53,6 +53,15 @@ $(window).scroll(function() {
 }).scroll();
 
 
+(function($){
+  'use strict';
+    $(window).on('load', function () {
+        if ($(".pre-loader").length > 0)
+        {
+            $(".pre-loader").fadeOut("slow");
+        }
+    });
+})(jQuery)
 
 
 
