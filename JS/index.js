1d750f65c147f7bbad2c4a4d02ad9203408f20b2
Nirmitjatana <Nirmitjatana@users.noreply.github.com>
2020-05-16 17:21:14 +0530

    faq section added

--- JS/index.js
+++ JS/index.js
@@ -4,3 +4,17 @@
 //       $('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top}, 300, 'linear');
 //     });
 //   });
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active3");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    }
+  });
+}
